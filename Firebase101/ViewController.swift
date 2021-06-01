b849fb7ef0fbf263a2a9d1c8e6a3f930b9b9280a
soduma <soduma@naver.com>
2021-06-01 15:53:15 +0900

    realtime database에 데이터 추가하기

--- Firebase101/ViewController.swift
+++ Firebase101/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         updateLabel()
+        saveBasicTypes()
+        saveCustomers()
     }
     
     func updateLabel() {
@@ -32,3 +34,51 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController {
+    func saveBasicTypes() {
+        db.child("int").setValue(1000)
+        db.child("dou").setValue(5.5)
+        db.child("str").setValue("ㅋㅋㅋㅋㅋ")
+        db.child("arr").setValue([1, 2, 3])
+        db.child("dic").setValue(["id" : "123", "age" : 29, "city" : "Seoul"])
+    }
+    
+    func saveCustomers() {
+        let books = [Book(title: "apple", author: "banana"), Book(title: "bus", author: "car")]
+        
+        let customer1 = Customer(id: "\(Customer.id)", name: "jang", books: books)
+        Customer.id += 1
+        let customer2 = Customer(id: "\(Customer.id)", name: "ki", books: books)
+        Customer.id += 1
+        let customer3 = Customer(id: "\(Customer.id)", name: "hwa", books: books)
+        Customer.id += 1
+        
+        db.child("Customers").child(customer1.id).setValue(customer1.toDictionary)
+        db.child("Customers").child(customer2.id).setValue(customer2.toDictionary)
+        db.child("Customers").child(customer3.id).setValue(customer3.toDictionary)
+    }
+}
+
+struct Customer {
+    let id: String
+    let name: String
+    let books: [Book]
+    
+    var toDictionary: [String : Any] {
+        let booksArray = books.map { $0.toDictionary }
+        let dict: [String : Any] = ["id" : id, "name" : name, "books" : booksArray]
+        return dict
+    }
+    
+    static var id: Int = 0
+}
+
+struct Book {
+    let title: String
+    let author: String
+    
+    var toDictionary: [String : Any] {
+        let dict: [String : Any] = ["title" : title, "author" : author]
+        return dict
+    }
+}
